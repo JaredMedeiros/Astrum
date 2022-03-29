de0a697cc1d88d2155bab78ebaf3153da3c9059d
JaredMedeiros <97901520+JaredMedeiros@users.noreply.github.com>
2022-03-28 23:25:25 -0400

    created footer

---
+++ Client/src/Components/PageFooter/PageFooter.js
@@ -0,0 +1,11 @@
+import './PageFooter.scss'
+
+export default function PageFooter() {
+    return (
+        <footer className = 'footer'>
+            <h2 className = 'footer__workblock-link'>
+                WorkBlock
+            </h2>
+        </footer>
+    )
+}
