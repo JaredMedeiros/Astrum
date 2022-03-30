83458d4e7820c37685d1cb5773b871088fb3e403
JaredMedeiros <97901520+JaredMedeiros@users.noreply.github.com>
2022-03-30 12:54:20 -0400

    fixed footer issues on mobile view and customizes scrollbars for any overflow

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,10 +1,28 @@
-import './App.css';
+import './App.scss';
 import LandingPage from './Components/LandingPage/LandingPage'
+import PageHeader from './Components/PageHeader/PageHeader'
+import ProjectListPage from './Components/ProjectListPage/ProjectListPage'
+import PageFooter from './Components/PageFooter/PageFooter'
+import AddProject from './Components/AddProject/AddProject';
+import AddTask from './Components/AddTask/AddTask';
+import AddTeam from './Components/AddTeam/AddTeam';
+import AddCheckpoint from './Components/AddCheckpoint/AddCheckpoint';
+import ProjectDashboard from './Components/ProjectDashboard/ProjectDashboard';
+import SprintRoadmap from './Components/SprintRoadmap/SprintRoadmap'
 
 function App() {
   return (
     <div className="App">
-      <LandingPage/>
+      {/* <LandingPage/> */}
+      <PageHeader/>
+      <SprintRoadmap/>
+      {/* <ProjectDashboard/> */}
+      {/* <AddCheckpoint/> */}
+      {/* <AddTeam/> */}
+      {/* <AddProject/> */}
+      {/* <AddTask/> */}
+      {/* <ProjectListPage/> */}
+      <PageFooter/>
     </div>
   );
 }
