5ecc66e57e66fb4b8f68b287db2483290ab0bc7a
JaredMedeiros <97901520+JaredMedeiros@users.noreply.github.com>
2022-03-29 23:01:51 -0400

    built project dashboard and made fully responsive

---
+++ Client/src/Components/ProjectDashboard/ProjectDashboard.js
@@ -0,0 +1,48 @@
+import './ProjectDashboard.scss'
+
+export default function ProjectDashboard() {
+    return (
+        // this name will be populated dynamically
+        <div className = 'project-dashboard'>
+            <h1 className = 'project-dashboard__title'>Project C</h1>
+            <main className = 'project-dashboard__entry-point-list'>
+                <div className = 'project-dashboard__entry-point'>
+                    <div className = 'project-dashboard__entry-point-top'>
+                        <div className = 'square'></div>
+                    </div>
+                    <div className = 'project-dashboard__entry-point-bottom'>
+                        <h2 className = 'project-dashboard__entry-point-title'>
+                            Task Board
+                        </h2>
+                    </div>
+                </div>
+                <div className = 'project-dashboard__entry-point'>
+                    <div className = 'project-dashboard__entry-point-top'>
+                        <div className = 'triangle'></div>
+                    </div>
+                    <div className = 'project-dashboard__entry-point-bottom'>
+                        <h2 className = 'project-dashboard__entry-point-title'>
+                            Chat
+                        </h2>
+                    </div>
+                </div>
+                <div className = 'project-dashboard__entry-point'>
+                    <div className = 'project-dashboard__entry-point-top'>
+                        <div className = 'circle'></div>
+                    </div>
+                    <div className = 'project-dashboard__entry-point-bottom'>
+                        <h2 className = 'project-dashboard__entry-point-title'>
+                            Sprint Roadmap
+                        </h2>
+                    </div>
+                </div>
+                <div className = 'add-team-button-mobile'>
+                <h3 className = 'add-team-button-mobile__text'>Team +</h3>
+            </div>
+            </main>
+            <div className = 'add-team-button'>
+                <h3 className = 'add-team-button__text'>Team +</h3>
+            </div>
+        </div>
+    )
+}
