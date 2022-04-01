c5dc4dafd7934471b9ca7d2c04f06793f6159311
JaredMedeiros <97901520+JaredMedeiros@users.noreply.github.com>
2022-04-01 00:25:18 -0400

    set up backend for logging in, signing up, and fetching user data, also connecting mongo db cluster

--- Server/server.js
+++ Server/server.js
@@ -4,6 +4,14 @@ const port = process.env.port || 5500;
 const connectDB = require('./config/db');
 
 connectDB();
+
 const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
